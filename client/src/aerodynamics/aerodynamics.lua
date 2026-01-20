6faa3966c78a9230ce84f56a752754a25e3d544c
karutimconver <karutimconver@gmail.com>
2026-01-20 09:15:38 +0000

    Update aerodynamics.lua

--- client/src/aerodynamics/aerodynamics.lua
+++ client/src/aerodynamics/aerodynamics.lua
@@ -7,6 +7,19 @@ local AIR_DENSITY = 0.4582725
 local THRUST = 770000
 local MOMENT_OF_INERTIA = 1625 -- aproximation. Might have to be calculated
 
+
+local function applyForce(force)
+    local direction = cpml.vec3.normalize(force)
+    local linearAcceleration = cpml.vec3.scale(direction, cpml.vec3.len(force) / MASS)
+
+    return linearAcceleration
+end
+
+local function applyTorque(torque)
+    local direction = cpml.vec3.normalize(torque)
+    local angularAccelaration = cpml.vec3.scale(direction, cpml.vec3.len(torque) / MOMENT_OF_INERTIA)
+end
+
 function aerodynamics(airfoils, forward, up, right, velocity)
     local totalForce = cpml.vec3.new(0, 0, 0)
     local pitchingMoment = 0
@@ -14,27 +27,20 @@ function aerodynamics(airfoils, forward, up, right, velocity)
 
     local AoA = math.acos(cpml.vec3.dot(forward, velocity) / (cpml.vec3.len(forward) * cpml.vec3.len(velocity)))
 
+    local aForce, aTorque
     for _, airfoil in pairs(airfoils) do
         aForce, aTorque = airfoil:calculateForcesAndTorque(velocity, right, AoA, AIR_DENSITY)
         totalForce = cpml.vec3.add(totalForce, aForce)
         pitchingMoment = pitchingMoment + aTorque
-        totalTorque = cpml.vec3.add(cpml.vec3.cross(airfoil:position(forward, up), totalForce), totalTorque) -- NEED TO TAKE CALCULATE AIRFOIL:POSITION!!!!!!!!!!
+        totalTorque = cpml.vec3.add(cpml.vec3.cross(airfoil:position(forward, up), aForce), totalTorque)
     end
 
     totalTorque = cpml.vec3.add(totalTorque, cpml.vec3.scale(cpml.vec3.normalize(right), -pitchingMoment))
 
     -- Calculate acceleration
 
+    local angularAccelaration = applyTorque(totalTorque)
+    local linearAcceleration = applyForce(totalForce)
 
-    local angularAccelaration = cpml.vec.scale()
-
-    return linearAcceleration
-end
-
-function applyForce(force)
-    local direction = cpml.vec3.normalize(force)
-    local linearAcceleration = cpml.vec3.scale(direction, cpml.vec3.len(force) / MASS)
-end
-
-function applyTorque()
+    return {linear = linearAcceleration, angular = angularAccelaration}
 end
