5ec023c2ca58c30e58e19ba5eaae447aad3748ef
karutimconver <137723911+karutimconver@users.noreply.github.com>
2026-09-16 23:19:02 +0100

    Revert "s"
    This reverts commit 39eca331018ec45740bbb8596c1a3b2474b93d56.

--- client/src/game.lua
+++ client/src/game.lua
@@ -44,7 +44,7 @@ local game = {
 
         self.buttons = {
             menu = {
-                Button(SCREEN_WIDTH/2, SCREEN_HEIGHT/2, 20, 20, "connect", "jogar")
+                Button(SCREEN_WIDTH/2, SCREEN_HEIGHT/2, 20, 20, "connect", "pila")
             },
 
             lobby = {
